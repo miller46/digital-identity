4f19232e8ca0c176bae5897c94c5ce83319ea4cf
Jack Miller <>
2017-09-22 17:49:10 -0500

    demo

--- main.js
+++ main.js
@@ -84,14 +84,32 @@ function beginScanner() {
     //     console.error(e);
     // });
 
+    var errBack = function() {
+        console.log("Error");
+    };
+
     var video = document.getElementById('preview');
 
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        // Not adding `{ audio: true }` since we only want video now
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
             video.src = window.URL.createObjectURL(stream);
             video.play();
         });
+    } else if (navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(mediaConfig, function(stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia(mediaConfig, function(stream){
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    } else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+        navigator.mozGetUserMedia(mediaConfig, function(stream){
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
     }
 }
 
