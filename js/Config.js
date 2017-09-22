150e09420a481d28180bb1deb1cc9b6d2de71fae
Jack Miller <>
2017-09-22 16:41:58 -0500

    demo

--- js/Config.js
+++ js/Config.js
@@ -2,7 +2,7 @@ module.exports = {
     isTestNet: true,
     networkName: "kovan",
     // baseUrl: "http://localhost:3000",
-    baseUrl: "https://digital-identity-app.herokuapp.com/",
+    baseUrl: "https://digital-identity-app.herokuapp.com",
     ethProvider: 'http://localhost:8545',
     etherscanApiKey: 'D3PZWYNWARKN73MCVWFGX1QZ6EWMJE7J45',
     personaRegistryContract: 'contracts/PersonaRegistry',
